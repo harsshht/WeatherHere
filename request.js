19799f155f0ab412f24dfd81418686708aecb4cd
Harshit shukla <106909875+harsshht@users.noreply.github.com>
2022-09-27 17:52:12 +0530

    Update request.js

--- request.js
+++ request.js
@@ -1,12 +1,12 @@
 const key = 'cbe3dd267a18f6c89943b3eff94f1ed7';
 
-// const baseURL = 'http://api.lopenweathermap.org/data/2.5/weather?q=Lagos&appid=cbe3dd267a18f6c89943b3eff94f1ed7';
+ const baseURL = 'http://api.lopenweathermap.org/data/2.5/weather?q=Lagos&appid=cbe3dd267a18f6c89943b3eff94f1ed7';
 
-// fetch(baseURL)
-//     .then((data) => { console.log('response', data.json()) })
-//     .catch((error) => {
-//         console.log(error);
-//     });
+ fetch(baseURL)
+     .then((data) => { console.log('response', data.json()) })
+     .catch((error) => {
+         console.log(error);
+     });
 
 const requestCity = async (city) => {
     const baseURL = 'http://api.openweathermap.org/data/2.5/weather'
